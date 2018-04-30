2a3bf7039265cd4b1bfcfc7cc94af20184df9da0
Melinx <Indra@Indra.local>
2018-04-30 12:59:15 +0200

    moving folders in react project

---
+++ vendor/jquery/3.3.1/scripts/main.js
@@ -0,0 +1,9 @@
+var $h1 = $('h1')
+
+
+$h1.text('Hello world');
+
+var h1 = $h1.text(); // to select h1 content, cuando esta vacio nos devuelve el texto, cuando lo escribimos lo estamos insertando.
+
+console.log('h1');
+
