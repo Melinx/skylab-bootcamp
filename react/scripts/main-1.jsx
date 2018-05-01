effc874abe721a6791cf783e39303015193ba706
Melinx <Indra@Indra.local>
2018-05-01 12:30:42 +0200

    react folders

---
+++ react/scripts/main-1.jsx
@@ -0,0 +1,19 @@
+'use strict'
+
+const title = 'Hello, World!'
+
+const a = 2, b = 3
+
+function sum(a, b) {
+    return a + b
+}
+
+const subtitle = <h2>Additionnnn</h2>
+
+// function subtraction
+// const main = <main>
+//     <h1>{title}</h1>
+//     2+3 = {sum(2,3)}
+// </main>
+
+// ReactDOM.render(main, document.getElementById('root'))
